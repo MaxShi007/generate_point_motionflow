6d1e49ca89ef344b9dda31261e4a39f0305ceb0a
zhangxiaojing <zhangxiaojing@haomo.ai>
2022-07-13 14:27:48 +0800

    .

--- flow2dataset.py
+++ flow2dataset.py
@@ -5,17 +5,21 @@ import os
 from tqdm import tqdm
 from icecream import ic
 import glob
+import yaml
+
 
 class Flow2Dataset(Flow2Residual):
-    
-    def __init__(self, config_file='flow2dataset.yaml'):
+
+    def __init__(self, config_file='flow2some.yaml'):
         super().__init__(config_file)
-        self.save_dataset=self.config['save_dataset']
-        self.save_dataset_root=self.config['save_dataset_root']
-        self.save_dataset_dir_name=self.config['save_dataset_dir_name']
-        self.non_ground_mask=self.config['non_ground_mask']
-        self.non_ground_mask_root=self.config['non_ground_mask_root']
-        self.dataset_form=self.config['dataset_form']
+        self.save_dataset = self.config['save_dataset']
+        self.save_dataset_root = self.config['save_dataset_root']
+        self.save_dataset_dir_name = self.config['save_dataset_dir_name']
+        self.non_ground_mask = self.config['non_ground_mask']
+        self.non_ground_mask_root = self.config['non_ground_mask_root']
+        self.velodyne_dir = self.config['velodyne_dir']
+        self.labels_dir = self.config['labels_dir']
+        self.flow_dir = self.config['flow_dir']
 
     def load_velo_data(self, velo_file_id):
         """
@@ -23,34 +27,32 @@ class Flow2Dataset(Flow2Residual):
         """
         velo_data = np.fromfile(self.velo_files[velo_file_id], dtype=np.float32).reshape(-1, 4)
         return velo_data
-    
-    def load_flow_data(self,velo_file_id):
+
+    def load_flow_data(self, velo_file_id):
         """
         Loads flow data
         """
-        flow_data = np.load(self.flow_files[velo_file_id])
+        path = self.flow_files[velo_file_id]
+        flow_data = np.load(path)
         # print(flow_data)
-        flow_data=(-flow_data) # 原本生成的flow应该是以current作为基点，last指向current的flow，但我们是要让网络估计current作为基点，current指向last的flow
+        flow_data = (-flow_data)  # 原本生成的flow应该是以current作为基点，last指向current的flow，但我们是要让网络估计current作为基点，current指向last的flow
         # print(flow_data)
         return flow_data
 
-    def save_nsfp_flow_dataset_file(self,current_point,last_point,flow_gt,velo_file_id,save_path,sequence):
+    def save_nsfp_flow_dataset_file(self, current_point, last_point, flow_gt, velo_file_id, save_path, sequence):
 
         if not os.path.exists(save_path):
             os.makedirs(save_path)
 
-        file_id=str(velo_file_id).zfill(6)
-        file_name=f'{sequence}_{file_id}.npz'
-        file_path=os.path.join(save_path,file_name)
-        np.savez(file_path,current_point=current_point,last_point=last_point,flow_gt=flow_gt)
+        file_id = str(velo_file_id).zfill(6)
+        file_name = f'{sequence}_{file_id}.npz'
+        file_path = os.path.join(save_path, file_name)
+        np.savez(file_path, current_point=current_point, last_point=last_point, flow_gt=flow_gt)
         print(file_path)
 
-    def load_nsfp_flow_dataset_file(self,file_path):
-        data=np.load(file_path)
-        return data['current_point'],data['last_point'],data['flow_gt']
-
-    def save_pointpwc_flow_dataset_file():
-        pass
+    def load_nsfp_flow_dataset_file(self, file_path):
+        data = np.load(file_path)
+        return data['current_point'], data['last_point'], data['flow_gt']
 
     def generate_dataset(self):
         for sequence in self.process_sequences_list:
@@ -58,8 +60,8 @@ class Flow2Dataset(Flow2Residual):
             print(f"Processing sequence {sequence}")
 
             flow_paths = os.path.join(self.data_path, 'sequences', sequence, self.flow_dir)
-            label_paths = os.path.join(self.data_path, 'sequences', sequence, 'labels')
-            velo_paths = os.path.join(self.data_path, 'sequences', sequence, 'velodyne')
+            label_paths = os.path.join(self.data_path, 'sequences', sequence, self.labels_dir)
+            velo_paths = os.path.join(self.data_path, 'sequences', sequence, self.velodyne_dir)
             # save_paths = os.path.join(self.data_path, 'sequences', sequence, self.save_dir_name)
 
             self.flow_files = self.get_flow_files(flow_paths)
@@ -68,85 +70,93 @@ class Flow2Dataset(Flow2Residual):
 
             assert len(self.flow_files) == len(self.label_files) and len(self.flow_files) == len(self.velo_files)
 
-            for velo_file_id in tqdm(range(1,len(self.velo_files))):
-                current_velo_data=self.load_velo_data(velo_file_id)[:,:3]
-                last_velo_data=self.load_velo_data(velo_file_id-1)[:,:3]
-                flow_gt=self.load_flow_data(velo_file_id)
+            for velo_file_id in tqdm(range(1, len(self.velo_files))):
+                current_velo_data = self.load_velo_data(velo_file_id)[:, :3]
+                last_velo_data = self.load_velo_data(velo_file_id - 1)[:, :3]
+                flow_gt = self.load_flow_data(velo_file_id)
                 # ic(current_velo_data,last_velo_data,flow_gt)
                 if self.save_dataset:
-                    assert self.dataset_form in ['nsfp','pointpwc']
-                    if self.dataset_form=='nsfp':
-                        save_path=os.path.join(self.save_dataset_root,self.save_dataset_dir_name)
-                        self.save_nsfp_flow_dataset_file(current_velo_data,last_velo_data,flow_gt,velo_file_id,save_path,sequence)
-                    else:
-                        # todo 保存为pointpwc格式
-                        print('还没写这呢')
-                        pass
 
+                    save_path = os.path.join(self.save_dataset_root, self.save_dataset_dir_name)
+                    self.save_nsfp_flow_dataset_file(current_velo_data, last_velo_data, flow_gt, velo_file_id, save_path, sequence)
 
     def remove_nsfp_ground_point(self):
-        dataset=os.path.join(self.save_dataset_root,self.save_dataset_dir_name)
+        dataset = os.path.join(self.save_dataset_root, self.save_dataset_dir_name)
         # print(dataset_path)
-        dataset_paths=sorted(glob.glob(os.path.join(dataset,'*.npz')))
+        dataset_paths = sorted(glob.glob(os.path.join(dataset, '*.npz')))
         # print(dataset_paths)
-        save_path=os.path.join(self.save_dataset_root,self.save_dataset_dir_name+'_non_ground_point') 
+        save_path = os.path.join(self.save_dataset_root, self.save_dataset_dir_name + '_non_ground_point')
         if not os.path.exists(save_path):
             os.makedirs(save_path)
 
         for dataset_path in tqdm(dataset_paths):
             print(f'remove ground point : {dataset_path}')
-            current_point,last_point,flow_gt=self.load_nsfp_flow_dataset_file(dataset_path)
+            current_point, last_point, flow_gt = self.load_nsfp_flow_dataset_file(dataset_path)
             # print(current_point.shape,last_point.shape,flow_gt.shape)
             if self.non_ground_mask:
-                sequence=dataset_path.split('/')[-1].split('_')[0]
-                current_label_file_name=dataset_path.split('/')[-1].split('_')[1].replace('.npz','.label')
-                current_non_ground_mask_path=os.path.join(self.non_ground_mask_root,f'{sequence}/ground_labels/{current_label_file_name}')
+                sequence = dataset_path.split('/')[-1].split('_')[0]
+                current_label_file_name = dataset_path.split('/')[-1].split('_')[1].replace('.npz', '.label')
+                current_non_ground_mask_path = os.path.join(self.non_ground_mask_root, f'{sequence}/ground_labels/{current_label_file_name}')
 
-                last_label_file_name=str(int(os.path.splitext(dataset_path.split('/')[-1].split('_')[1])[0])-1).zfill(6)+'.label'
-                last_non_ground_mask_path=os.path.join(self.non_ground_mask_root,f'{sequence}/ground_labels/{last_label_file_name}')
+                last_label_file_name = str(int(os.path.splitext(dataset_path.split('/')[-1].split('_')[1])[0]) - 1).zfill(6) + '.label'
+                last_non_ground_mask_path = os.path.join(self.non_ground_mask_root, f'{sequence}/ground_labels/{last_label_file_name}')
 
                 # ic(current_non_ground_mask_path,last_non_ground_mask_path)
 
-                current_non_ground_mask_data=np.fromfile(current_non_ground_mask_path,dtype=np.uint32).reshape(-1)
-                last_non_ground_mask_data=np.fromfile(last_non_ground_mask_path,dtype=np.uint32).reshape(-1)
+                current_non_ground_mask_data = np.fromfile(current_non_ground_mask_path, dtype=np.uint32).reshape(-1)
+                last_non_ground_mask_data = np.fromfile(last_non_ground_mask_path, dtype=np.uint32).reshape(-1)
                 current_sem_label = current_non_ground_mask_data & 0xFFFF
                 last_sem_label = last_non_ground_mask_data & 0xFFFF
-                current_non_ground_mask=(current_sem_label!=9)
-                last_non_ground_mask=(last_sem_label!=9)
-                flow_gt_non_ground_mask=current_non_ground_mask
+                current_non_ground_mask = (current_sem_label != 9)
+                last_non_ground_mask = (last_sem_label != 9)
+                flow_gt_non_ground_mask = current_non_ground_mask
 
                 # ic(np.unique(current_non_ground_mask,return_counts=True),np.unique(last_non_ground_mask,return_counts=True))
 
-                
-                
-
-
             else:
-                current_non_ground_mask=current_point[:,2]>-1.4
-                last_non_ground_mask=last_point[:,2]>-1.4
-                flow_gt_non_ground_mask=current_non_ground_mask
+                current_non_ground_mask = current_point[:, 2] > -1.4
+                last_non_ground_mask = last_point[:, 2] > -1.4
+                flow_gt_non_ground_mask = current_non_ground_mask
 
             # assert(current_non_ground_mask.sum()<86000 and last_non_ground_mask.sum()<86000)
-            
-            current_point=current_point[current_non_ground_mask]
-            last_point=last_point[last_non_ground_mask]
-            flow_gt=flow_gt[flow_gt_non_ground_mask]
+
+            current_point = current_point[current_non_ground_mask]
+            last_point = last_point[last_non_ground_mask]
+            flow_gt = flow_gt[flow_gt_non_ground_mask]
             # print(current_point.shape,last_point.shape,flow_gt.shape)
-            file_name=dataset_path.split('/')[-1]
-            save_file_path=os.path.join(save_path,file_name)
+            file_name = dataset_path.split('/')[-1]
+            save_file_path = os.path.join(save_path, file_name)
             # print(save_file_path)
             #todo保存为nsfp格式
-            np.savez(save_file_path,current_point=current_point,last_point=last_point,flow_gt=flow_gt)
+            np.savez(save_file_path, current_point=current_point, last_point=last_point, flow_gt=flow_gt)
             print(save_file_path)
             #todo保存为pointpwc格式
 
 
+# class FromNonGroundGenerateDataset(Flow2Dataset):
+#     def __init__(self,data_path,config,split,save_file):
+#         self.data_path=data_path
+#         self.config=yaml.safe_load(open(config, "r"))
+#         self.process_sequences_list=self.config["split"][split]
+#         print("process_sequences_list:", self.process_sequences_list)
+#         self.save_file = save_file
 
+#     def from_non_ground_generate_dataset(self, process_flow_dir):
+#         for sequence in self.process_sequences_list:
+#             sequence = str(sequence).zfill(2)
+#             print(f"Processing sequence {sequence}")
 
+#             label_paths = os.path.join(self.data_path, 'sequences', sequence, 'non_ground_labels')
+#             velo_paths = os.path.join(self.data_path, 'sequences', sequence, 'non_ground_velodyne')
+#             flow_paths = os.path.join(self.data_path, 'sequences', sequence, process_flow_dir)
 
+#             self.flow_files = self.get_flow_files(flow_paths)
+#             self.label_files = self.get_label_files(label_paths)
+#             self.velo_files = self.get_velo_files(velo_paths)
 
+#             assert len(self.flow_files) == len(self.label_files) and len(self.flow_files) == len(self.velo_files)
 
-if __name__=="__main__":
-    semantic_kitti=Flow2Dataset(config_file='flow2some.yaml')
-    # semantic_kitti.generate_dataset()
-    semantic_kitti.remove_nsfp_ground_point()
+if __name__ == "__main__":
+    semantic_kitti = Flow2Dataset(config_file='flow2some.yaml')
+    semantic_kitti.generate_dataset()
+    # semantic_kitti.remove_nsfp_ground_point()
--- remove_ground_for_everything.py
+++ remove_ground_for_everything.py
@@ -11,12 +11,14 @@ class RemoveGroundForEverything(object):
         self.data_root = data_root
         self.non_ground_mask_root = non_ground_mask_root
         self.semantickitti_cfg = yaml.safe_load(open(semantickitti_cfg, "r"))
+        assert process_split in ["train", "valid", "test"]
         self.process_sequences_list = self.semantickitti_cfg['split'][process_split]
         print("process_sequences_list:", self.process_sequences_list)
         self.save_file = save_file
         self.save_dir_name = save_dir_name
         self.velo_files = []
         self.label_files = []
+        self.flow_files = []
 
     def get_label_files(self, label_paths):
         """
@@ -31,6 +33,14 @@ class RemoveGroundForEverything(object):
         Gets velo files
         """
         files = [os.path.join(root, file) for root, dir, files in os.walk(velo_paths) for file in files]
+        files.sort()
+        return files
+
+    def get_flow_files(self, flow_paths):
+        """
+        Gets flow files
+        """
+        files = [os.path.join(root, file) for root, dir, files in os.walk(flow_paths) for file in files]
         files.sort()
         return files
 
@@ -56,12 +66,20 @@ class RemoveGroundForEverything(object):
         label_data = np.fromfile(self.label_files[label_file_id], dtype=np.uint32).reshape(-1)
         return label_data
 
+    def load_flow(self, flow_file_id):
+        """
+        Loads flow data
+        """
+        flow_data = np.load(self.flow_files[flow_file_id])
+        return flow_data
+
     def load_mask(self, mask_file_id):
         """
         Loads mask data
         """
         mask_data = np.fromfile(self.non_ground_mask_files[mask_file_id], dtype=np.uint32).reshape(-1)
-        non_ground_mask = (mask_data != 9)
+        sem_mask_data = mask_data & 0xFFFF
+        non_ground_mask = (sem_mask_data != 9)
         return non_ground_mask
 
     def remove_ground_for_velo(self):
@@ -69,11 +87,13 @@ class RemoveGroundForEverything(object):
             sequence = str(sequence).zfill(2)
             print(f"Processing sequence {sequence}")
 
-            self.velo_paths = os.path.join(self.data_root, 'sequences', sequence, 'velodyne')
-            self.non_ground_mask_paths = os.path.join(self.non_ground_mask_root, sequence, "ground_labels")
+            save_dir = os.path.join(self.data_root, "sequences", sequence, self.save_dir_name + "velodyne")
 
-            self.non_ground_mask_files = self.get_mask_files(self.non_ground_mask_paths)
-            self.velo_files = self.get_velo_files(self.velo_paths)
+            velo_paths = os.path.join(self.data_root, 'sequences', sequence, 'velodyne')
+            non_ground_mask_paths = os.path.join(self.non_ground_mask_root, sequence, "ground_labels")
+
+            self.non_ground_mask_files = self.get_mask_files(non_ground_mask_paths)
+            self.velo_files = self.get_velo_files(velo_paths)
 
             assert len(self.non_ground_mask_files) == len(self.velo_files)
 
@@ -84,19 +104,94 @@ class RemoveGroundForEverything(object):
                 assert len(velo_data) == len(non_ground_mask), "error!!! velo file unmatched with mask file"
 
                 non_ground_velo = velo_data[non_ground_mask]
-                #todo 存文件
+
+                if save_file:
+                    if not os.path.exists(save_dir):
+                        os.makedirs(save_dir)
+
+                    non_ground_velo_file = self.velo_files[id].replace(velo_paths, save_dir)
+                    non_ground_velo.tofile(non_ground_velo_file)
+                else:
+                    print(non_ground_velo)
 
     def remove_ground_for_label(self):
-        pass
+        for sequence in self.process_sequences_list:
+            sequence = str(sequence).zfill(2)
+            print(f"Processing sequence {sequence}")
+
+            save_dir = os.path.join(self.data_root, "sequences", sequence, self.save_dir_name + "labels")
+
+            label_paths = os.path.join(self.data_root, 'sequences', sequence, 'labels')
+            non_ground_mask_paths = os.path.join(self.non_ground_mask_root, sequence, "ground_labels")
+
+            self.non_ground_mask_files = self.get_mask_files(non_ground_mask_paths)
+            self.label_files = self.get_label_files(label_paths)
+
+            assert len(self.non_ground_mask_files) == len(self.label_files)
+
+            for id in tqdm(range(len(self.label_files))):
+                label_data = self.load_label(id)
+                non_ground_mask = self.load_mask(id)
+
+                assert len(label_data) == len(non_ground_mask), "error!!! label file unmatched with mask file"
+
+                non_ground_label = label_data[non_ground_mask]
+
+                if save_file:
+                    if not os.path.exists(save_dir):
+                        os.makedirs(save_dir)
+
+                    non_ground_label_file = self.label_files[id].replace(label_paths, save_dir)
+                    non_ground_label.tofile(non_ground_label_file)
+                else:
+                    print(non_ground_label)
+
+    #todo 把flow的地面点也移除了，这样我就有完整的 无地面点数据，随意组合生成sceneflow训练要用的数据集格式
+    def remove_ground_for_flow(self, flow_dir_name):
+        for sequence in self.process_sequences_list:
+            sequence = str(sequence).zfill(2)
+            print(f"Processing sequence {sequence}")
+
+            save_dir = os.path.join(self.data_root, "sequences", sequence, self.save_dir_name + flow_dir_name)
+
+            flow_paths = os.path.join(self.data_root, 'sequences', sequence, flow_dir_name)
+            non_ground_mask_paths = os.path.join(self.non_ground_mask_root, sequence, "ground_labels")
+
+            self.non_ground_mask_files = self.get_mask_files(non_ground_mask_paths)
+            self.flow_files = self.get_flow_files(flow_paths)
+
+            assert len(self.non_ground_mask_files) == len(self.flow_files)
+
+            for id in tqdm(range(len(self.flow_files))):
+                flow_data = self.load_flow(id)
+                non_ground_mask = self.load_mask(id)
+
+                assert len(flow_data) == len(non_ground_mask), "error!!! flow file unmatched with mask file"
+
+                non_ground_flow = flow_data[non_ground_mask]
+
+                if save_file:
+                    if not os.path.exists(save_dir):
+                        os.makedirs(save_dir)
+
+                    non_ground_flow_file = self.flow_files[id].replace(flow_paths, save_dir)
+                    np.save(non_ground_flow_file, non_ground_flow)
+                else:
+                    print(non_ground_flow.shape)
+                    print(non_ground_flow)
 
 
 if __name__ == "__main__":
     data_root = "/share/sgb/semantic_kitti/dataset/"
     non_ground_mask_root = "/share/sgb/kitti-ground"
     semantickitti_cfg = "./semantic-kitti.yaml"
-    process_split = 'train'
-    save_file = False
-    save_dir_name = "non_ground_"
+    flow_dir_name = "motionflow_egomotion_4DMOS_POSES_1"  # 去掉哪个flow的地面点
+    process_split = 'train'  # train  valid  test
+
+    save_file = True
+    save_dir_name = "non_ground_"  # 为了适配non_ground_velodyne  non_ground_label，提供一个统一的命名头
 
     remove_ground = RemoveGroundForEverything(data_root, non_ground_mask_root, semantickitti_cfg, process_split, save_file, save_dir_name=save_dir_name)
     remove_ground.remove_ground_for_velo()
+    remove_ground.remove_ground_for_label()
+    remove_ground.remove_ground_for_flow(flow_dir_name=flow_dir_name)
